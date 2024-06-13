dd414266dcb52c96cd711a2da26cb575d5e15958
elestayani <estayaniellord74@gmail.com>
2024-06-14 02:04:28 +0800

    Created the Navigation for the Doctor.

---
+++ react-app/src/Patient/components/Navigation.js
@@ -0,0 +1,44 @@
+import React from "react";
+import useAuth from "../../hooks/useAuth";
+import { NavLink } from "react-router-dom";
+
+const Navigation = () => {
+    const { logout } = useAuth();
+
+    return (
+        <nav className="bg-dark navbar navbar-expand-lg d-flex justify-content-between align-items-center p-3">
+            <h4 className="text-white text-uppercase m-0">
+                Hospital Management System
+            </h4>
+            <ul className="navbar-nav fs-6 gap-4">
+                <li className="nav-item position-relative">
+                    <NavLink
+                        to="medical-records"
+                        className="nav-link text-white text-center fw-bold px-3 rounded-2"
+                    >
+                        Medical Records
+                    </NavLink>
+                </li>
+                <li className="nav-item position-relative">
+                    <NavLink
+                        to="doctors"
+                        className="nav-link text-white text-center fw-bold px-3 rounded-2"
+                    >
+                        List of Doctors
+                    </NavLink>
+                </li>
+                <li className="nav-item position-relative">
+                    <NavLink
+                        to="appointment"
+                        className="nav-link text-white text-center fw-bold px-3 rounded-2"
+                    >
+                        Appointments
+                    </NavLink>
+                </li>
+            </ul>
+            <button onClick={logout}>Logout</button>
+        </nav>
+    );
+};
+
+export default Navigation;
