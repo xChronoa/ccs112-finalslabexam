326892cc0e50282c2b189de8e4c62b4c5df7800c
Vince Fontanilla <fontanillavincecarlo95@gmail.com>
2024-06-14 01:11:17 +0800

    Created Patient Migration

---
+++ laravel/database/migrations/2024_06_12_112621_create_patients_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->date('date_of_birth');
+            $table->string('gender');
+            $table->string('address');
+            $table->string('phone');
+            $table->string('email')->unique();
+            $table->string('emergency_contact');
+            $table->text('medical_history')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('patients');
+    }
+};
